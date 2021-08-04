c3327b102a3e10dfb643f85679ffd802b2a250c6
Viktoreja <mogilinez17@gmail.com>
2021-08-04 11:27:31 +0300

    Start work

---
+++ KonsolKrista/Presentation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolKrista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("jkfndn");
+            Console.ReadKey();
+        }
+    }
+}
